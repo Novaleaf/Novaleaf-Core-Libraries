9b683ec6beb69a8ef81ed35e4f423234791a0b28
Jason Swearingen <jasons.github@novaleaf.com>
2013-02-20 18:59:35 +0700

    add xsd for ng schema validation

--- dev-libs/angular/install-xhtml5-ng-to-vs2012.cmd
+++ dev-libs/angular/install-xhtml5-ng-to-vs2012.cmd
@@ -10,19 +10,38 @@
 @echo .
 
 @rem @echo off
-set thisPath=%0
 
+@rem @echo ------------------------------------
+@rem @echo SET UP ENVIRONMENT HELPERS
+@rem @echo ------------------------------------
+
+
+@rem get location of the batch file
+set thisPath=%0
 FOR /F %%I IN ("%0") DO SET thisDir=%%~dpI
+
+@rem get date
+FOR /F "TOKENS=1* DELIMS= " %%A IN ('DATE/T') DO SET CDATE=%%B
+FOR /F "TOKENS=1,2 eol=/ DELIMS=/ " %%A IN ('DATE/T') DO SET mm=%%B
+FOR /F "TOKENS=1,2 DELIMS=/ eol=/" %%A IN ('echo %CDATE%') DO SET dd=%%B
+FOR /F "TOKENS=2,3 DELIMS=/ " %%A IN ('echo %CDATE%') DO SET yyyy=%%B
+SET date=%yyyy%%mm%%dd%
+
+@rem @echo ------------------------------------
+@rem @echo SET UP PURPOSE HELPERS
+@rem @echo ------------------------------------
 
 set targetDir=%VS110COMNTOOLS%..\Packages\schemas\html\
 set targetFile=xhtml_5.xsd
 set sourceDir=%thisDir%
 set sourceFile=xhtml_5-ng.xsd
+set targetBackupFile=%targetFile%.%date%.original
 
 @echo ------------------------------------
 @echo PURPOSE OF THIS FILE:  install our xhtml5 schema with ng bindings into visual studio 2012.
-@echo     you need to run this as administrator, and need to restart visual studio afterwards
+@echo     you need to run this as administrator, and need to restart visual studio afterwards to take effect
 @echo ------------------------------------
+pause
 @echo .
 @echo .
 
@@ -35,8 +54,11 @@ set sourceFile=xhtml_5-ng.xsd
 
 
 
-@echo backup original (if asks to overwrite, you should say no!)
-copy "%targetDir%%targetFile%" "%targetDir%%targetFile%.ng.original" /-Y
+@echo backup original to %targetDir%%targetBackupFile%
+@echo .
+@echo == SAY NO IF ASKED TO OVERWRITE! == 
+@echo .
+copy "%targetDir%%targetFile%" "%targetDir%%targetBackupFile%" /-Y
 @echo copy, overwriting xhtml5 schema 
 xcopy "%sourceDir%%sourceFile%" "%targetDir%%targetFile%"  /Y
 
@@ -48,5 +70,4 @@ xcopy "%sourceDir%%sourceFile%" "%targetDir%%targetFile%"  /Y
 @echo .
 @echo .
 
-pause
 exit 0
